2783ebe637cfa097eea1ff84534dab9d8b591377
Firstname Lastname <chou@simmons.edu>
2020-05-17 02:21:20 -0400

    Import components to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Playlist from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
 
 function App() {
   return (
