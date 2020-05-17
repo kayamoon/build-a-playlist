8a4786ef8b8ee9188c12017815e962e22ec4e8e8
Firstname Lastname <chou@simmons.edu>
2020-05-17 02:20:05 -0400

    Create Tracklist component

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Tracklist.css';
+import Track from '../Track/Track';
+
+class Tracklist extends React.Component {
+    render() {
+        return(
+            <div className="TrackList">
+                
+            </div>
+        );
+    }
+}
+
+export default Tracklist;
