1172363a848105c593cf6e5d1411826d92b34854
Firstname Lastname <chou@simmons.edu>
2020-05-21 21:51:46 -0400

    Add comments to Search Results component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SearchResults';
-import Tracklist from '../TrackList/TrackList';
+import Tracklist from '../Tracklist/Tracklist';
 
 class SearchResults extends React.Component {
     render() {
         return(
             <div className="SearchResults">
                 <h2>Results</h2>
-
+                Add a tracklist component
             </div>
         )
     }
