69baf845e0dab5e21f53897fdb7df3dde667531d
Firstname Lastname <chou@simmons.edu>
2020-05-25 19:08:27 -0400

    Update the state of term in handleTermChange

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -16,14 +16,14 @@ class SearchBar extends React.Component {
     }
 
     handleTermChange(event){
-        this.setState({ search: event.target.value });
+        this.setState({ term: event.target.value });
     }
 
     render() {
         return(
             <div className="SearchBar">
                 <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
-                <button className="SearchButton">SEARCH</button>
+                <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         );
     }
