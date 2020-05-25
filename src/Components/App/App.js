19cd4b0630ee0a25e143a45f8242e379660fb9c7
Firstname Lastname <chou@simmons.edu>
2020-05-24 22:19:11 -0400

    Allow user to change name of playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -49,6 +49,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track){
@@ -70,6 +71,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks });
   }
 
+  updatePlaylistName(name){
+    this.setState({ playlistName: name });
+  }
+
   render(){
     return (
       <div>
@@ -81,7 +86,7 @@ class App extends React.Component {
               onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks}
-              onRemove={this.removeTrack}/>
+              onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
