5525d926f0af77751d9f655c67d0f2e44f9cefc9
Firstname Lastname <chou@simmons.edu>
2020-05-26 16:36:59 -0400

    Add ability to search on key press (enter)

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -9,6 +9,7 @@ class SearchBar extends React.Component {
         }
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
     search(){
@@ -19,10 +20,17 @@ class SearchBar extends React.Component {
         this.setState({ term: event.target.value });
     }
 
+    handleKeyDown(event){
+        if(event.keyCode === 13){
+            this.props.onSearch(this.state.term);
+            event.preventDefault(); //prevents the default action of clicking a link from triggering at the end of the method
+        }
+    }
+
     render() {
         return(
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyDown={this.handleKeyDown}/>
                 <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         );
